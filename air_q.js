6bab646cfcc2b019ea73eed95b9f04b57e30fbea
Base Zero <vacapp@live.com>
2023-03-15 11:00:04 +0700

    Create air_q.js

---
+++ air_q.js
@@ -0,0 +1,20 @@
+const YOUR_API_KEY = '';
+const CITY_NAME='ban-chang';
+const STATE_NAME = 'rayong';
+const COUNTRY_NAME = 'thailand';
+
+const URL = `http://api.airvisual.com/v2/city?city=${CITY_NAME}&state=${STATE_NAME}&country=${COUNTRY_NAME}&key=${YOUR_API_KEY}`;
+
+function myFunction() {
+  console.log(URL);
+  var response = UrlFetchApp.fetch(URL);
+  var json = response.getContentText();
+  var data = JSON.parse(json);
+  Logger.log(json);
+  Logger.log(data);
+  const r = data.data;  
+  var result = `${r.city} ${r.state} ${r.country} `
+             + `${r.current.pollution.aqius} `
+             + `${r.location.coordinates[0]} ${r.location.coordinates[0]}`;
+  Logger.log(result); 
+}
